b31412d3e84f7980c5b6708f476c30268fd20f04
timaimee <wy_ljl@163.com>
2016-05-24 21:30:24 +0800

    add the other way to realize

--- src/com/timaimee/threeHundred/LeetCode344.java
+++ src/com/timaimee/threeHundred/LeetCode344.java
@@ -7,11 +7,24 @@ package com.timaimee.threeHundred;
  */
 public class LeetCode344 {
 	public static void main(String[] args) {
-		System.out.println(reverseString("string"));
+		System.out.println(reverseStringByChar("string"));
 	}
 
+	//5ms
 	public static String reverseString(String s) {
 		StringBuffer sb = new StringBuffer(s);
 		return sb.reverse().toString();
+	}
+	//4ms
+	public static String reverseStringByChar(String s) {
+		char[] ch = s.toCharArray();
+		char temp = '0';
+		for (int i = 0; i < ch.length / 2; i++) {
+			temp = ch[i];
+			ch[i] = ch[ch.length - 1-i];
+			ch[ch.length - 1-i] = temp;
+		}
+		return String.valueOf(ch);
+
 	}
 }
