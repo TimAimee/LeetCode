31affeb6fba1afe397d5295d0bd745ad7ae15d14
timaimee <wy_ljl@163.com>
2016-05-25 22:57:54 +0800

    swap

--- src/com/timaimee/threeHundred/LeetCode345.java
+++ src/com/timaimee/threeHundred/LeetCode345.java
@@ -1,8 +1,32 @@
 package com.timaimee.threeHundred;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class LeetCode345 {
 	public static void main(String[] args) {
+		//the two ways is same , --> a e i o u<--, and swap
+		System.out.println(reverseVowelsByCaikehe("A man a plan a canal: Panama"));
 		System.out.println(reverseVowelsbySCHBS("A man a plan a canal: Panama"));
+	}
+
+	public static String reverseVowelsByCaikehe(String s) {
+		Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+		char[] chars = s.toCharArray();
+		int l = 0, r = chars.length - 1;
+		while (l < r) {
+			while (l < r && !vowels.contains(Character.toLowerCase(chars[l]))) {
+				l++;
+			}
+			while (l < r && !vowels.contains(Character.toLowerCase(chars[r]))) {
+				r--;
+			}
+			char tmp = chars[l];
+			chars[l++] = chars[r];
+			chars[r--] = tmp;
+		}
+		return new String(chars);
 	}
 
 	public static String reverseVowelsbySCHBS(String s) {
