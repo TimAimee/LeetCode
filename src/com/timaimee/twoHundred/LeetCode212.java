0e1e4a35cf4655099ac1d069f77295da387bc6d9
timaimee <wy_ljl@163.com>
2016-05-26 22:16:12 +0800

    test

---
+++ src/com/timaimee/twoHundred/LeetCode212.java
@@ -0,0 +1,13 @@
+package com.timaimee.twoHundred;
+
+public class LeetCode212 {
+	public static void main(String[] args) {
+
+		findString("oath");
+	}
+
+	static void findString(String str) {
+		char[][] chArr = new char[][] { { 'o', 'a', 'a', 'n' }, { 'e', 't', 'a', 'e' }, { 'i', 'h', 'k', 'r' },
+				{ 'i', 'f', 'l', 'v' } };
+	}
+}
