b39ffec7dfac97ca0ec94e9c88b6583c6b22e750
timaimee <wy_ljl@163.com>
2016-05-27 23:35:38 +0800

    has 3 way to sovle the 283

--- src/com/timaimee/twoHundred/LeetCode283.java
+++ src/com/timaimee/twoHundred/LeetCode283.java
@@ -2,6 +2,8 @@ package com.timaimee.twoHundred;
 
 import java.util.ArrayList;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 /**
  * @author timaimee
  * @date 2016-05-27 22:56
@@ -11,9 +13,25 @@ import java.util.ArrayList;
 public class LeetCode283 {
 	public static void main(String[] args) {
 		int[] arr = new int[] { 0, 0, 0, 2, 0, 1, 2 };
-		moveZeroes(arr);
+		moveZero(arr);
 	}
 
+	// 1ms
+	public static void moveZero(int[] nums) {
+		int p1 = 0;
+		int p2 = 0;
+		for (; p1 < nums.length; p1++) {
+			if (nums[p1] != 0) {
+				nums[p2] = nums[p1];
+				p2++;
+			}
+		}
+		for (; p2 < nums.length; p2++) {
+			nums[p2] = 0;
+		}
+	}
+
+	// 3ms
 	public static void moveZeroes(int[] nums) {
 		ArrayList<Integer> arrList = new ArrayList<Integer>();
 		for (int i = 0; i < nums.length; i++) {
@@ -28,4 +46,28 @@ public class LeetCode283 {
 
 	}
 
+	// 24ms
+	public static void moveZeroess(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				for (int j = i; j < nums.length; j++) {
+					if (nums[j] != 0) {
+						int temp = nums[i];
+						nums[i] = nums[j];
+						nums[j] = temp;
+						break;
+					}
+				}
+				getStr(nums);
+			}
+		}
+
+	}
+
+	public static void getStr(int[] nums) {
+		System.out.print("\n");
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + ",");
+		}
+	}
 }
