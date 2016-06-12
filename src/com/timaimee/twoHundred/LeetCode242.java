9589ac4385b9cb6211a2172cf1db682b1e600d37
timaimee <wy_ljl@163.com>
2016-06-12 22:58:15 +0800

    add the other way to sovle the 204

--- src/com/timaimee/twoHundred/LeetCode242.java
+++ src/com/timaimee/twoHundred/LeetCode242.java
@@ -23,4 +23,21 @@ public class LeetCode242 {
 
 		return new String(sChar).equals(new String(tChar));
 	}
+	
+	 public boolean isAnagramOtherWay(String s, String t) {
+	        int[] table = new int[26];
+	        // 记录字母在第一个单词中出现的次数
+	        for(int i = 0; i < s.length(); i++){
+	            table[s.charAt(i)-'a']++;
+	        }
+	        // 减去字母在第二个单词中出现的次数
+	        for(int i = 0; i < t.length(); i++){
+	            table[t.charAt(i)-'a']--;
+	        }
+	        // 如果某个计数器不为0，则返回假
+	        for(int i = 0; i < 26; i++){
+	            if(table[i]!=0) return false;
+	        }
+	        return true;
+	    }
 }
